f8e1727f6798412cfd4a2179b7d971148579fa16
Brian Obot <brianobot9@gmail.com>
2022-12-29 12:41:37 +0100

    remove django url tag from form action attribute in index and about page

--- js/main.js
+++ js/main.js
@@ -10,8 +10,14 @@ function close_darkscreen(){
     console.log(dark_screen.classList)
 }
 
-function searchPage(query){
-    find();
+
+function searchPageFunction(search_input_id){
+    console.log("Starting search function...");
+    console.log("Search Input Id = ", search_input_id);
+    query = document.getElementById(search_input_id).value; 
+    console.log("Search Key = ", query);
+    last_input = query[-1];
+    console.log(last_input);
 }
 
 /**
