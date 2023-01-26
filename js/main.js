58b503c0348530c84d335fc675c119b43cb7dc74
Brian Obot <brianobot9@gmail.com>
2023-01-26 17:35:36 +0100

    Initial commit for faq section

--- js/main.js
+++ js/main.js
@@ -37,6 +37,27 @@ function toggleTheme(){
     }
 })();
 
+
+// Frequently asked question section script
+let faq = document.getElementsByClassName("faq-question");
+
+let i;
+for (i = 0; i < faq.length; i++) {
+    faq[i].addEventListener("click", function () {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        console.log('clicked faq');
+        this.classList.toggle("active");
+        /* Toggle between hiding and showing the active panel */
+        var body = this.nextElementSibling;
+        if (body.style.display === "block") {
+            body.style.display = "none";
+        } else {
+            body.style.display = "block";
+        }
+    });
+}
+
 /**
 * Javascript used for Navbar gotten from...
 * Template Name: Medilab - v4.7.1
